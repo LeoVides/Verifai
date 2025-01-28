d7cdc724043d48948ac60b4f96308417dbe86640
Mathieu COQUELET <coqueletmathieu@gmail.com>
2025-01-28 13:12:51 +0100

    add search page and submit search action

---
+++ app/javascript/controllers/result_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="result"
+export default class extends Controller {
+  static targets = ["fullResult", "form"]
+
+  connect() {
+  }
+
+  // Shows the full result
+  compute(event) {
+    event.preventDefault();
+    console.log('compute');
+    this.fullResultTarget.classList.remove('d-none');
+
+    fetch(this.formTarget.action, {
+      method: "POST",
+      headers: { "Accept": "application/json" },
+      body: new FormData(this.formTarget)
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.user_input) {
+          this.fullResultTarget.innerHTML = `<h2>Result</h2><div>${data.user_input}</div>`;
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error)
+      })
+  }
+
+  resetForm() {
+    console.log("Resetting form inputs")
+    this.formTarget.reset() // Clear all fields in the form
+  }
+}
