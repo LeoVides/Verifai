6940381bc79e4b0056f6a8484ba9057945a25126
Leonardo Vides <leovides9@gmail.com>
2025-01-29 17:57:34 +0100

    improve results layout

--- app/javascript/controllers/result_controller.js
+++ app/javascript/controllers/result_controller.js
@@ -28,28 +28,50 @@ export default class extends Controller {
         if (data.user_input) {
           this.fullResultTarget.classList.remove('d-none');
           this.fullResultTarget.innerHTML = `
-            <p><strong>Answer</strong></p>
             <div>
-              ${data.user_input}
+              <p><strong>Input</strong></p>
+              <p>${data.user_input}</p>
             </div>
             <div class="row">
-              <div class="col col-6 col-md-4 col-lg-4">
+              <div class="col col-md-6 col-lg-6">
                 <div class="card-saved">
-                  <div class="row-saved">
+                  <div class="row-card">
                     <h2>Political bias</h2>
                     <p class="tag">${data.political_bias}</p>
                   </div>
                 </div>
               </div>
-              <div class="col col-6 col-md-4 col-lg-4">
+              <div class="col col-md-6 col-lg-6">
                 <div class="card-saved">
-                  <div class="row-saved">
+                  <div class="row-card">
                     <h2>Fact score</h2>
                     <p class="tag">${data.fact_score}</p>
                   </div>
                 </div>
               </div>
-            </div>`;
+            </div>
+            <p><strong>Sources</strong></p>
+            <div class="row">
+              <div class="col col-6 col-md-4 col-lg-4">
+                <div class="card-saved">
+                    <h2>Title of article</h2>
+                    <p>Title of page</p>
+                </div>
+              </div>
+              <div class="col col-6 col-md-4 col-lg-4">
+                <div class="card-saved">
+                    <h2>Title of article</h2>
+                    <p>Title of page</p>
+                </div>
+              </div>
+              <div class="col col-6 col-md-4 col-lg-4">
+                <div class="card-saved">
+                    <h2>Title of article</h2>
+                    <p>Title of page</p>
+                </div>
+              </div>
+            </div>
+          `;
         }
       })
       .catch((error) => {
