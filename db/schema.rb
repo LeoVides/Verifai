537bfe1084678abada2443053b1ea5a341097af6
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-01-29 15:07:19 +0100

    remove political bias confidence

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_28_101001) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_29_132718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_28_101001) do
     t.string "political_bias"
     t.string "source_media"
     t.text "user_input"
-    t.float "political_bias_confidence"
     t.float "fact_score_confidence"
     t.string "title"
     t.bigint "user_id", null: false
