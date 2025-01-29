f03b32dbc058cb6c6bc269bd189a9753dc411508
Leonardo Vides <leovides9@gmail.com>
2025-01-29 18:47:29 +0100

    work in progress

--- app/javascript/controllers/result_controller.js
+++ app/javascript/controllers/result_controller.js
@@ -30,12 +30,11 @@ export default class extends Controller {
         console.log(data);
         if (data.user_input) {
           this.fullResultTarget.classList.remove('d-none');
-      
+
           let mediaList = "";
           Object.entries(data.media).forEach(([key, value]) => {
             console.log(key, value);
             mediaList += `<li><strong>${key}:</strong> <a href="${value}" target="_blank">${value}</a></li>`;
-
           });
 
           this.fullResultTarget.innerHTML = `
@@ -44,52 +43,28 @@ export default class extends Controller {
               <p>${data.user_input}</p>
             </div>
             <div class="row">
-              <div class="col col-md-6 col-lg-6">
+              <div class="col col-lg-6">
                 <div class="card-saved">
-                  <div class="row-card">
+                  <div class="row-card mt-0">
                     <h2>Political bias</h2>
                     <p class="tag">${data.political_bias}</p>
                   </div>
                 </div>
               </div>
-              <div class="col col-md-6 col-lg-6">
+              <div class="col col-lg-6">
                 <div class="card-saved">
-                  <div class="row-card">
+                  <div class="row-card mt-0">
                     <h2>Fact score</h2>
                     <p class="tag">${data.fact_score}</p>
                   </div>
                 </div>
               </div>
             </div>
-            <p><strong>Sources</strong></p>
-            <div class="row">
-              <div class="col col-6 col-md-4 col-lg-4">
-                <div class="card-saved">
-                    <h2>Title of article</h2>
-                    <p>Title of page</p>
-                </div>
-              </div>
-              <div class="col col-6 col-md-4 col-lg-4">
-                <div class="card-saved">
-                    <h2>Title of article</h2>
-                    <p>Title of page</p>
-                </div>
-              </div>
-              <div class="col col-6 col-md-4 col-lg-4">
-                <div class="card-saved">
-                    <h2>Title of article</h2>
-                    <p>Title of page</p>
-                </div>
-              </div>
-            </div>
             <div>
-              <strong>Fact score:</strong>${data.fact_score}.
               <strong>Read from other sources:<ul>${mediaList}</ul>
             </div>
           `;
-
           this.fullResultTarget.insertAdjacentHTML('afterend', `<div class="alert alert-success alert-dismissible fade show m-1" role="alert">Success! Your now have ${data.user_checker_score} checker points! <i class="fa-solid fa-thumbs-up fa-bounce fa-lg"></i></div>`);
-
         }
       })
       .catch((error) => {
