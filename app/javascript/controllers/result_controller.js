22c0b62eeac45c7b402b0c7b48c6ce05f936d8fd
Mathieu COQUELET <coqueletmathieu@gmail.com>
2025-02-04 15:09:27 +0100

    advanced version of result page

--- app/javascript/controllers/result_controller.js
+++ app/javascript/controllers/result_controller.js
@@ -36,7 +36,7 @@ export default class extends Controller {
                                                                   <div class="loader1"></div>
                                                                 </div>`);
         // Show a flash message
-        this.fullResultTarget.insertAdjacentHTML("beforeend", `<div class="alert alert-info" role="alert">
+        this.fullResultTarget.insertAdjacentHTML("beforeend", `<div class="alert alert-info alert-dismissible fade show m-1" role="alert" data-controller="flash" data-flash-target="message">
           Processing your request... Please wait.
         </div>`);
 
