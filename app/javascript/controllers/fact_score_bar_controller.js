e8956bfce4016f35d18d4d6aa47691c4b1341ad8
Mathieu COQUELET <coqueletmathieu@gmail.com>
2025-02-04 12:49:28 +0100

    new components for the search page

---
+++ app/javascript/controllers/fact_score_bar_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="fact-score-bar"
+export default class extends Controller {
+  connect() {
+    const progressBar = this.element;
+    const targetWidth = progressBar.getAttribute("aria-valuenow");
+
+    progressBar.style.width = "0%"; // Start from 0
+    setTimeout(() => {
+      progressBar.style.transition = "width 1.5s ease-out";
+      progressBar.style.width = targetWidth + "%"; // Animate to target width
+    }, 200);
+  }
+}
